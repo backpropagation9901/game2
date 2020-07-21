871904b99bba14a59a811c3791350afc330013b3
backpropagation9901 <68401741+backpropagation9901@users.noreply.github.com>
2020-07-21 11:40:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 var charicter = document.getElementById("charicter");
 var block = document.getElementById("block");
 function jump(){
-character.classList.add("animate");
-setTimeout(function(){
-character.classList.remove("animate");
+    if(character.classList !="animate"){
+      character.classList.add("animate");
+    }
+  setTimeout(function(){
+    character.classList.remove("animate");
 },500);
 
 }
+
+var checkDead = setInterval(function(){
+  var charicterTop = 
+      parseInt(windown.getComputedStyle(charicter).
+               getPropertyValue("top"));
+  var blockLeft = 
+      parseInt(windown.getComputedStyle(block).
+               getPropertyValue("left"));
+  if(blockLeft<20 && blockLeft>0 && charicterTop>=130){
+    block.style.animation = "none";
+    block.style.display = "none";
+    alert("You Are Weak!");
+  }
+    
+},10);
