92c5cf666a4e45053be938e818462043b7cc73f2
backpropagation9901 <68401741+backpropagation9901@users.noreply.github.com>
2020-07-21 10:52:40 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+ #javaScript 
+var charicter = documentgetElementById("charicter");
+var block = document.getElementBYId("block");
+function jump(){
+character.classList.add("animate");
+setTimeout(function(){
+character.classList.remove("animate");
+},500);
+
+}
